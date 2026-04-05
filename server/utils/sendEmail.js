877888739b626fa4adab5f6804184a398bc23dfd
22SOECE11080 <radheshjoshi02@gmail.com>
2026-04-05 16:58:27 +0530

    one more change

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,9 @@ const sendEmail = async (to, subject, text) => {
     console.log(`📧 Attempting to send email to: ${to}`);
     
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
